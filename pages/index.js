7328929631832006482e01096a64a9b8545523b1
N37R09U3 <n37r09u3@gmail.com>
2023-02-16 00:38:17 +0800

    add Header.jsx

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
+          <Header />
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
